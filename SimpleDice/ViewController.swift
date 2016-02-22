9dc68ed120372628c6501b105502434706f10ffd
Joe Kawai <joe@teamkarbon.com>
2016-02-22 23:51:19 +0800

    Initial Commit

---
+++ SimpleDice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleDice
+//
+//  Created by JOE KAWAI on 22/2/16.
+//  Copyright © 2016 TeamKarbon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
